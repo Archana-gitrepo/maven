20dff1bc01ab07401125c79b60a82510fbbb8357
archansethu <achu.sethu.24@gmail.com>
2025-06-26 11:35:29 +0530

    arithmetic exception

---
+++ Maven_Project/src/main/java/exception/ArithmeticException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class ArithmeticException {
+
+	public static void main(String[] args) {
+		System.out.println("Program Starting");
+		System.out.println(10/0);
+		System.out.println("Program Ending");
+	}
+
+}
